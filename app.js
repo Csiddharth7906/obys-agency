ef078595f4e44fb099aa9b91c2c14bfcfc8852c4
Siddharth Chauhan <siddharthchauhan7906@gmail.com>
2025-05-21 21:37:45 +0530

    page1 done

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+function loadingAnimation(){
+
 
 let tl=gsap.timeline()
 tl.from(".line h1",{
@@ -38,9 +40,34 @@ tl.from("#page1",{
     delay:0.2,
     y:1600,
     opacity:0,
-    duration:0.5,
+    duration:0.6,
     ease:Power4 
 })
 tl.to("#loader",{
   display:"none"
 })
+tl.from("#nav",{
+    opacity:0
+    
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3   h2,#hero4 h1 ",{
+    y:120,
+   stagger:0.25 
+    
+})
+
+}
+
+function cursorAnimation(){
+
+document.addEventListener("mousemove",function(dets){
+   gsap.to("#crsr",{
+    left:dets.x,
+    top:dets.y
+   })
+ 
+})
+Shery.makeMagnet("#nav-part2 h4" )
+}
+cursorAnimation();
+loadingAnimation();   
